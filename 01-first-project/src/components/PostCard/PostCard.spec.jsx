93d9f1c8dd98c98fb963c8d358a61939f083dfd0
Pedro Santana <pedro.ferraz01@gmail.com>
2023-03-17 19:44:16 -0300

    more unit tests

---
+++ 01-first-project/src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,21 @@
+const { render, screen } = require("@testing-library/react");
+const { PostCard } = require(".");
+const { PostCardPropsMock } = require("./mock");
+
+const props = PostCardPropsMock;
+
+describe("<PostCard/>", () => {
+  it("should render PostCArd correctly", () => {
+    // render(<PostCard title="algo" cover="url" body="Some text" id="1" />);
+    render(<PostCard {...props} />);
+
+    expect(screen.getByRole("img", { name: /TITLE1/i })).toHaveAttribute("src", "img/img.png");
+    expect(screen.getByRole("heading", { name: "title1 1" })).toBeInTheDocument();
+    expect(screen.getByText("body1")).toBeInTheDocument();
+  });
+  it("should match the snapshot", () => {
+    // render(<PostCard title="algo" cover="url" body="Some text" id="1" />);
+    const { container } = render(<PostCard {...props} />);
+    expect(container).toMatchSnapshot();
+  });
+});
